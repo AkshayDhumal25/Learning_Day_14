f64feac8b0f8c094f679aa31b00d08771c022fe6
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-04-02 09:23:00 +0530

    seond largest number

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿// 1. WAP to find Second largest element in an array : arr[] = [12, 35, 1, 10, 34, 1, 35], without
+// sorting, without using any built-in methods and without deleting duplicate elements. What will be
+// the time complexity?
+using System;
+namespace logicals
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 12, 35, 1, 10, 34, 1, 35 };
+            int secLarge = 0;
+            int large = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > large)
+                {
+                    secLarge = large;
+                    large = arr[i];
+                }
+                if (arr[i] < large && arr[i] > secLarge)
+                {
+                    secLarge = arr[i];
+                }
+            }
+            Console.WriteLine(secLarge);
+        }
+    }
+}
