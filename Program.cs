fa70f9d068d814b4d5d6d5141f1a0b0bcacc37d6
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-04-02 11:01:54 +0530

    WAP for getting a square root of a given number.

--- Program.cs
+++ Program.cs
@@ -255,43 +255,59 @@
 //}
 
 // 11. Find prime numbers from 1 ....n
+//using System;
+//using System.Linq;
+//using System.Collections.Generic;
+//namespace logicals
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter the number to get the Prime Numbers : ");
+//            int n = Convert.ToInt32(Console.ReadLine());
+//            var list = new List<int>();
+//            for (int i = 2; i < n; i++)
+//            {
+//                if (isPrime(i))
+//                {
+//                    list.Add(i);
+//                }
+//            }
+//            foreach (var i in list)
+//            {
+//                Console.WriteLine(i);
+//            }
+//        }
+//        public static bool isPrime(int n)
+//        {
+//            if (n < 2) return false;
+//            if (n == 2) return true;
+//            if (n % 2 == 0) return false;
+//            for (int i = 3; i * i < n; i += 2)
+//            {
+//                if (n % i == 0)
+//                {
+//                    return false;
+//                }
+//            }
+//            return true;
+//        }
+//    }
+//}
+
+// 12. WAP for getting a square root of a given number.
 using System;
-using System.Linq;
-using System.Collections.Generic;
 namespace logicals
 {
-    class Program
+    class Prime
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter the number to get the Prime Numbers : ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            var list = new List<int>();
-            for (int i = 2; i < n; i++)
-            {
-                if (isPrime(i))
-                {
-                    list.Add(i);
-                }
-            }
-            foreach (var i in list)
-            {
-                Console.WriteLine(i);
-            }
-        }
-        public static bool isPrime(int n)
-        {
-            if (n < 2) return false;
-            if (n == 2) return true;
-            if (n % 2 == 0) return false;
-            for (int i = 3; i * i < n; i += 2)
-            {
-                if (n % i == 0)
-                {
-                    return false;
-                }
-            }
-            return true;
+            Console.WriteLine("Enter the number : ");
+            double n = Convert.ToInt32(Console.ReadLine());
+            double res = Math.Sqrt(n);
+            Console.WriteLine(res);
         }
     }
 }
